9d082218d7033b6413ee9b3d9e1f404cc2e48809
brian <brianmstricker@gmail.com>
2024-09-03 22:20:25 -0400

    chore: folder structure/setup

---
+++ server/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+ res.send("Hello, world!!!");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+ console.log(`Server is running on port ${port}`);
+ console.log("updated");
+});
