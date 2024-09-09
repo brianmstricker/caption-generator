f0e6d0074b23815c0d77397216ce8e93a6b94d49
brian <brianmstricker@gmail.com>
2024-09-09 14:51:15 -0400

    up: input accept (video), uploadcard animation

--- client/src/components/home/UploadCard.vue
+++ client/src/components/home/UploadCard.vue
@@ -1,14 +1,38 @@
+<script lang="ts" setup>
+const uploadVideo = (event: Event) => {
+ const file = (event.target as HTMLInputElement).files?.[0];
+ if (!file) return;
+ if (!file.type.startsWith("video/")) {
+  console.error("Only video files are allowed");
+  return;
+ }
+ const formData = new FormData();
+ formData.append("video", file);
+ fetch("http://localhost:5000/upload", {
+  method: "POST",
+  body: formData,
+ })
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.error(error));
+};
+</script>
+
 <template>
- <div
-  class="h-[50vh] sm:h-[60vh] min-h-[400px] w-[180px] md:w-[260px] bg-black/40 rounded-xl flex items-center justify-center overflow-hidden border border-white/10 transition-all duration-200"
-  id="upload-card"
- >
-  <button
-   class="bg-orange-500 px-4 py-2 rounded-full md:text-xl font-semibold hover:[boxShadow:0_0_32px_0_rgba(255,255,255,0.15)] transition-all duration-200 hover:scale-105"
-   id="upload-card-button"
-   aria-label="Upload video"
+ <div class="slide">
+  <div
+   class="h-[50vh] sm:h-[60vh] min-h-[400px] w-[180px] md:w-[260px] bg-black/40 rounded-xl flex items-center justify-center overflow-hidden border border-white/10 transition-all duration-200"
+   id="upload-card"
   >
-   Upload video
-  </button>
+   <label
+    class="bg-orange-500 px-4 py-2 rounded-full md:text-xl font-semibold hover:[boxShadow:0_0_32px_0_rgba(255,255,255,0.15)] transition-all duration-200 hover:scale-105 cursor-pointer"
+    id="upload-card-button"
+    aria-label="Upload video"
+    for="upload-card-input"
+   >
+    Upload video
+   </label>
+   <input type="file" class="hidden" id="upload-card-input" @change="uploadVideo($event)" accept="video/*" />
+  </div>
  </div>
 </template>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -3,7 +3,7 @@ import UploadCard from "../components/home/UploadCard.vue";
 </script>
 
 <template>
- <div class="flex-1 flex flex-col justify-center items-center gap-10">
+ <div class="flex-1 flex flex-col justify-center items-center gap-10 overflow-hidden">
   <div>
    <h1 class="text-4xl sm:text-6xl text-balance text-center">Add captions to your videos!</h1>
    <p class="mt-1 text-center text-[15px] sm:text-lg text-neutral-200">Upload a video, wait a moment, and voila! Captions are generated!</p>
